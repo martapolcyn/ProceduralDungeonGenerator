eee50b9f38d63365c6a92179d407b5092542f17e
martapolcyn <59393197+martapolcyn@users.noreply.github.com>
2025-04-28 19:55:42 +0200

    Basic dungeon

---
+++ Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralDungeonGenerator
+{
+    internal class Room
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Room(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}
