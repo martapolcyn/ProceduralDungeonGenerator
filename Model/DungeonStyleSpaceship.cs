fde5726930c5272e22e6442196876fc04a936784
martapolcyn <59393197+martapolcyn@users.noreply.github.com>
2025-05-11 00:34:25 +0200

    Add dungeon style interface

---
+++ Model/DungeonStyleSpaceship.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralDungeonGenerator.Model
+{
+    internal class DungeonStyleSpaceship : IDungeonStyle
+    {
+        public Pen GetCorridorPen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color GetRoomColor(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoomShape GetRoomShape(Room room)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Model/IDungeonStyle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralDungeonGenerator.Model
+{
+    internal interface IDungeonStyle
+    {
+        RoomShape GetRoomShape(Room room);
+        Color GetRoomColor(Room room);
+        Pen GetCorridorPen();
+    }
+}
