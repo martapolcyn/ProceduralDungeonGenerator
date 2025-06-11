3f971fac8c565181af9e622f92f796e3c10afee0
martapolcyn <59393197+martapolcyn@users.noreply.github.com>
2025-06-11 18:27:56 +0200

    Making itemclass abstract

---
+++ Model/ArchitecturalItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralDungeonGenerator.Model
+{
+    public class ArchitecturalItem : Item
+    {
+        public override ItemCategory Category => ItemCategory.Architecture;
+
+        public ArchitecturalItem(string id, string style, string name, RoomType roomType, PlacementType placement, int weight)
+            : base(id, style, name, roomType, placement, weight)
+        {
+        }
+    }
+}
